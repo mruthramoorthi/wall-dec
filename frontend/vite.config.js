99144e340cd4f15760664bafd984c56e90ddbefd
mruthramoorthi <mruthramoorthi66@gmail.com>
2026-08-17 22:13:12 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000',
-      '/images': 'http://localhost:4000',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:4000',
+          changeOrigin: true,
+        },
+        '/images': {
+          target: env.VITE_API_URL || 'http://localhost:4000',
+          changeOrigin: true,
+        },
+      },
     },
-  },
+  };
 });
